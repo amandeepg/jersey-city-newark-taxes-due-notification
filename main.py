ab53bfac7622cf92feb0bf2177318db100081c9b
Amandeep Grewal <me@amandeep.ca>
2021-01-03 03:52:26 -0500

    Format

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ from bs4 import BeautifulSoup
 
 ACCOUNT_NUMBER = os.environ.get('ACCOUNT_NUMBER')
 DAYS_THRESHOLD = int(os.environ.get('DAYS_THRESHOLD', '31'))
-FROM_EMAIL = os.environ.get('FROM_EMAIL')
-TO_EMAIL = os.environ.get('TO_EMAIL')
+FROM_EMAIL = Email(os.environ.get('FROM_EMAIL'))
+TO_EMAIL = To(os.environ.get('TO_EMAIL'))
 EMAIL_SUBJECT = os.environ.get('EMAIL_SUBJECT')
 SG = sendgrid.SendGridAPIClient(api_key=os.environ.get('SENDGRID_API'))
 
+
 def sentence_case(str):
     return '. '.join(i.capitalize() for i in str.split('. '))
+
 
 def url_get_table(url):
     req = urllib.request.Request(url=url)
@@ -24,13 +26,9 @@ def url_get_table(url):
     p.feed(f.read().decode('utf-8'))
     return p.tables
 
-# print("\n\nPANDAS DATAFRAME\n")
-# import pandas
-# print(pandas.DataFrame(tables[0]))
-# print("\n\nPANDAS DATAFRAME END\n")
 
 def check(event, context):
-    TODAY = date.today()
+    today = date.today()
     url = f'http://taxes.cityofjerseycity.com/ViewPay?accountNumber={ACCOUNT_NUMBER}'
     tables = url_get_table(url)
 
@@ -46,7 +44,7 @@ def check(event, context):
             pass
 
         if open_balance_dollars > 0.0:
-            days_diff = (datetime.strptime(due_date, '%m/%d/%Y').date() - TODAY).days
+            days_diff = (datetime.strptime(due_date, '%m/%d/%Y').date() - today).days
             if days_diff < DAYS_THRESHOLD:
                 output += f'<u>{description}</u> of <u>{open_balance}</u> due on <u>{due_date}</u><br/>\n'
 
@@ -55,6 +53,14 @@ def check(event, context):
         plain_text = BeautifulSoup(output).get_text()
         print(plain_text)
 
-        SG.send(message=Mail(Email(FROM_EMAIL), To(TO_EMAIL), EMAIL_SUBJECT, Content("text/plain", plain_text), HtmlContent(output)))
+        SG.send(
+            message=Mail(
+                FROM_EMAIL,
+                TO_EMAIL,
+                EMAIL_SUBJECT,
+                Content("text/plain", plain_text),
+                HtmlContent(output)
+            )
+        )
 
     return output
